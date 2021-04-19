9b43214675afab184937727d241114cf41b7defc
user4 <user4@mail.ru>
2021-04-19 09:31:11 +0700

    g

--- app/src/main/java/com/example/clicker/MainActivity.kt
+++ app/src/main/java/com/example/clicker/MainActivity.kt
@@ -5,19 +5,34 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var tap = findViewById<Button>(R.id.button8)
+//        var tap = findViewById<Button>(R.id.button8)
+//
+//        tap.setOnClickListener { v ->
+//            val perehod = Intent(this, EgraActivity::class.java)
+//            startActivity(perehod)
+//            v.setBackgroundColor(Color.GRAY)
+//            }
 
-        tap.setOnClickListener { v ->
-            val perehod = Intent(this, EgraActivity::class.java)
-            startActivity(perehod)
-            v.setBackgroundColor(Color.GRAY)
-            }
+
+        val g: BottomNavigationView = findViewById(R.id.nav_view)
+
+        val nc = findNavController(R.id.nav_host_fragment)
+
+        val abc = AppBarConfiguration(setOf(
+            R.id.egraActivity, R.id.shop))
+        setupActionBarWithNavController(nc, abc)
+        g.setupWithNavController(nc)
         }
 
     }
--- app/src/main/java/com/example/clicker/Shop.kt
+++ app/src/main/java/com/example/clicker/Shop.kt
@@ -4,30 +4,31 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.fragment.app.Fragment
 
-class Shop : AppCompatActivity() {
-    val buttonlefttop : Button by lazy { findViewById<Button>(R.id.button6) }
-    val buttonmiddletop : Button by lazy { findViewById<Button>(R.id.button7) }
-    val buttonrighttop : Button by lazy { findViewById<Button>(R.id.button8) }
-    val buttonexit : Button by lazy { findViewById<Button>(R.id.button9) }
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_shop)
-
-        buttonlefttop.setOnClickListener {
-            Data.vid = 2
-        }
-        buttonmiddletop.setOnClickListener {
-            Data.vid = 3
-        }
-        buttonrighttop.setOnClickListener {
-            Data.vid = 5
-        }
-        buttonexit.setOnClickListener {
-            val back = Intent(this, EgraActivity::class.java)
-            startActivity(back)
-        }
-    }
+class Shop : Fragment() {
+//    val buttonlefttop : Button by lazy { findViewById<Button>(R.id.button6) }
+//    val buttonmiddletop : Button by lazy { findViewById<Button>(R.id.button7) }
+//    val buttonrighttop : Button by lazy { findViewById<Button>(R.id.button8) }
+//    val buttonexit : Button by lazy { findViewById<Button>(R.id.button9) }
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_shop)
+//
+//        buttonlefttop.setOnClickListener {
+//            Data.vid = 2
+//        }
+//        buttonmiddletop.setOnClickListener {
+//            Data.vid = 3
+//        }
+//        buttonrighttop.setOnClickListener {
+//            Data.vid = 5
+//        }
+//        buttonexit.setOnClickListener {
+//            val back = Intent(this, EgraActivity::class.java)
+//            startActivity(back)
+//        }
+//    }
 
 
 }
